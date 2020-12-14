c2abcb008944e402a2d6b94206fec7c0a4a97ac3
ReBarrington <reagan@barringtonfamily.net>
2020-12-14 09:50:48 -0500

    initial

---
+++ getUserData/index.js
@@ -0,0 +1,11 @@
+// lambda aws extension makes boilerplate
+
+'use strict'
+const AWS = require('aws-sdk');
+
+exports.handler = function (event, context, callback) {
+    console.log(JSON.stringify(`Event: event`))
+    // Lambda Code Here
+    // context.succeed('Success!')
+    // context.fail('Failed!')
+}
