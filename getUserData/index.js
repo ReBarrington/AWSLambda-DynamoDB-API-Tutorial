1432e1c7ec139fff9525bbff53ec260fe7fa940f
ReBarrington <reagan@barringtonfamily.net>
2020-12-14 10:27:48 -0500

    updating

--- getUserData/index.js
+++ getUserData/index.js
@@ -1,11 +1,22 @@
 // lambda aws extension makes boilerplate.
+// handler will be index.handler
+// this tutorial is using the dynamoDB
+
 
 'use strict'
 const AWS = require('aws-sdk');
 
+// set region
+AWS.config.update({ region: "us-east-1" })
+
 exports.handler = function (event, context, callback) {
-    console.log(JSON.stringify(`Event: event`))
-    // Lambda Code Here
-    // context.succeed('Success!')
-    // context.fail('Failed!')
+    const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
+    const params = {
+        TableName: "Users",
+        Key: {
+            id: {
+                s: "12345"
+            }
+        }
+    }
 }
